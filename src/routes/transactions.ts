59de8771fdd940729c2a66f3b602fea141f32dd2
Tarcisio Philips <tarcisiogabriel.philips@gmail.com>
2023-12-10 15:27:51 -0300

    feat: list all transactions and lista transaction by id

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -5,6 +5,25 @@ import {z} from 'zod'
 
 export async function transactionsRoutes(app: FastifyInstance) {  
 
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select('*')
+
+    return {transactions}
+  })
+
+  app.get('/:id', async (request) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const {id} = getTransactionParamsSchema.parse(request.params)
+
+    const transaction = await knex('transactions').where('id', id).first()
+
+    return {transaction}
+
+  })
+
   app.post('/', async (request, reply) => {
 
     const createTransactionBodySchema = z.object({
@@ -21,6 +40,8 @@ export async function transactionsRoutes(app: FastifyInstance) {
         title,
         amount: type === 'credit' ? amount : amount*-1,
       })
+
+
 
 
     return reply.status(201).send()
