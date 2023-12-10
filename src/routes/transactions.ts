677a8e59acf4c9497fcd9202ef48a3b09f53a567
Tarcisio Philips <tarcisiogabriel.philips@gmail.com>
2023-12-10 15:09:23 -0300

    feat: creating transactions

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,14 +1,29 @@
-
 import { FastifyInstance } from 'fastify'
 import { knex } from '../database'
+import crypto from 'node:crypto'
+import {z} from 'zod'
 
-export async function transactionsRoutes(app: FastifyInstance) {
-  
-  app.get('/hello', async () => {
-    const transaction = await knex('transactions')
-      .select('*')
-      .where('amount', 1000)
-    return transaction
+export async function transactionsRoutes(app: FastifyInstance) {  
+
+  app.post('/', async (request, reply) => {
+
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit'])
+    })
+
+    const {title, amount, type} = createTransactionBodySchema.parse(request.body)
+
+    await knex('transactions')
+      .insert({
+        id: crypto.randomUUID(),
+        title,
+        amount: type === 'credit' ? amount : amount*-1,
+      })
+
+
+    return reply.status(201).send()
   })
   
 }
