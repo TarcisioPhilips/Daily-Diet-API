049daf7960692e56f502df1b0c2fb93ca388892e
Tarcisio Philips <tarcisiogabriel.philips@gmail.com>
2023-12-09 21:43:23 -0300

    feat: first queries using knex

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import fastify from 'fastify'
 import { knex } from './database'
+import crypto from 'node:crypto'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
-  return tables
+  const transaction = await knex('transactions')
+    .select('*')
+    .where('amount', 1000)
+  return transaction
 })
 
 app
