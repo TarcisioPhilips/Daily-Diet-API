e5b8981eb2cd38c1db2a90161d4954e7223e75bd
Tarcisio Philips <tarcisio.philips@escoladnc.com.br>
2023-12-14 10:11:12 -0300

    chore: creating users and meals routes

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,21 @@
 import fastify from 'fastify'
 import cookie from '@fastify/cookie'
 
-import { env } from './env'
-import {transactionsRoutes} from './routes/transactions'
+import {userRoutes} from './routes/users'
+import { mealsRoutes } from './routes/meals'
 
 export const app = fastify()
 
 app.register(cookie)
 
-// app.addHook('preHandler', async (request, reply) => {
-//   console.log(`[${request.method}] ${request.url}`)
-// })
+app.addHook('preHandler', async (request, reply) => {
+  console.log(`[${request.method}] ${request.url}`)
+})
 
-app.register(transactionsRoutes, {
-  prefix: 'transactions',
+app.register(userRoutes, {
+  prefix: 'users',
+})
+
+app.register(mealsRoutes, {
+  prefix: 'meals',
 })
